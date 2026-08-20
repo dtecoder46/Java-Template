67491cb824edd4b3c2f7fbd8028496cad43beac4
Daniel <dte.biotech46@gmail.com>
2026-08-20 19:08:18 -0400

    Add Main class with main method and print function

---
+++ Main.java
@@ -0,0 +1,9 @@
+public class Main {
+  public static void main(String[] args) {
+    print("Hello world");  
+  }
+  
+  public static void print(String text) {
+    System.out.println(text);
+  }
+}
